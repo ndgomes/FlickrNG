9e7a972d042e74491b557478846cf3accd26805e
Nuno Gomes <nuno98gomes@gmail.com>
2024-06-22 13:48:03 +0200

    [feat] added comments on server side code

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const axios = require("axios");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-dotenv.config();
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(cors()); // Enable CORS for all routes
 
 const flickrApiKey = process.env.FLICKR_API_KEY;
 const flickrBaseUrl = "https://www.flickr.com/services/rest/";
@@ -23,14 +23,16 @@ const flickr = axios.create({
   params: defaultParams,
 });
 
+// Route to get images from Flickr API
 app.get("/getImages", async (req, res) => {
-  const query = req.query.query;
-  const perPage = req.query.perPage || 24;
-  const page = req.query.page || 1;
+  const query = req.query.query; // Get search query from request parameters
+  const perPage = req.query.perPage || 24; // Get number of images per page, default to 24
+  const page = req.query.page || 1; // Get current page number, default to 1
 
-  const userAgent = req.headers["user-agent"] || "Mozilla/5.0";
+  const userAgent = req.headers["user-agent"] || "Mozilla/5.0"; // Get User-Agent header to avoid an API call error
 
   try {
+    // Make request to Flickr API to get images with the desired query
     const response = await flickr.get("/?method=flickr.photos.search", {
       params: {
         text: query,
@@ -44,6 +46,7 @@ app.get("/getImages", async (req, res) => {
 
     const data = response.data;
 
+    // Map API response to desired format
     const images = data.photos.photo.map((photo) => ({
       id: photo.id,
       title: photo.title,
@@ -51,6 +54,7 @@ app.get("/getImages", async (req, res) => {
       originalUrl: `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
     }));
 
+    // Send response with image data and other information
     res.json({
       images,
       total: data.photos.total,
